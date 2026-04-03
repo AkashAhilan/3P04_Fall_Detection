80af06ee2aeb024a80b8f4d8f4fcc5a9e04dc8e9
Akash Ahilan <173340746+AkashAhilan@users.noreply.github.com>
2026-04-03 00:50:31 -0400

    Workking 2 Load Cells Reading Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,4 @@
-
-
-/*
-   Settling time (number of samples) and data filtering can be adjusted in the
-   config.h file For calibration and storing the calibration value in eeprom,
-   see example file "Calibration.ino"
-
-   The update() function checks for new data and starts the next conversion. In
-   order to acheive maximum effective sample rate, update() should be called at
-   least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS. If you
-   have other time consuming code running (i.e. a graphical LCD), consider
-   calling update() from an interrupt routine, see example file
-   "Read_1x_load_cell_interrupt_driven.ino".
-
-   This is an example sketch on how to use this library
-*/
-
+// Working File With Both Outputs.
 #include <Arduino.h>
 #include <HX711_ADC.h>
 #if defined(ESP8266) || defined(ESP32) || defined(AVR)
@@ -22,13 +6,19 @@
 #endif
 
 // pins:
-const int HX711_dout = 34;  // mcu > HX711 dout pin
-const int HX711_sck = 25;   // mcu > HX711 sck pin
+const int HX711_dout_1 = 34;  // mcu > HX711 no 1 dout pin
+const int HX711_sck_1 = 25;   // mcu > HX711 no 1 sck pin
+const int HX711_dout_2 = 35;  // mcu > HX711 no 2 dout pin
+const int HX711_sck_2 = 26;   // mcu > HX711 no 2 sck pin
 
-// HX711 constructor:
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
+// HX711 constructor (dout pin, sck pin)
+HX711_ADC LoadCell_1(HX711_dout_1, HX711_sck_1);  // HX711 1
+HX711_ADC LoadCell_2(HX711_dout_2, HX711_sck_2);  // HX711 2
 
-const int calVal_calVal_eepromAdress = 0;
+const int calVal_eepromAdress_1 =
+    0;  // eeprom adress for calibration value load cell 1 (4 bytes)
+const int calVal_eepromAdress_2 =
+    4;  // eeprom adress for calibration value load cell 2 (4 bytes)
 unsigned long t = 0;
 
 void setup() {
@@ -37,67 +27,72 @@ void setup() {
   Serial.println();
   Serial.println("Starting...");
 
-  float calibrationValue;  // calibration value
-  calibrationValue =
+  float calibrationValue_1;  // calibration value load cell 1
+  float calibrationValue_2;  // calibration value load cell 2
+
+  calibrationValue_1 =
       696.0;  // uncomment this if you want to set this value in the sketch
+  calibrationValue_2 =
+      733.0;  // uncomment this if you want to set this value in the sketch
 #if defined(ESP8266) || defined(ESP32)
   // EEPROM.begin(512); // uncomment this if you use ESP8266 and want to fetch
-  // this value from eeprom
+  // the value from eeprom
 #endif
-  // EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you
-  // want to fetch this value from eeprom
+  // EEPROM.get(calVal_eepromAdress_1, calibrationValue_1); // uncomment this if
+  // you want to fetch the value from eeprom EEPROM.get(calVal_eepromAdress_2,
+  // calibrationValue_2); // uncomment this if you want to fetch the value from
+  // eeprom
 
-  LoadCell.begin();
-  // LoadCell.setReverseOutput();
+  LoadCell_1.begin();
+  LoadCell_2.begin();
+  // LoadCell_1.setReverseOutput();
+  // LoadCell_2.setReverseOutput();
   unsigned long stabilizingtime =
       2000;  // tare preciscion can be improved by adding a few seconds of
              // stabilizing time
   boolean _tare = true;  // set this to false if you don't want tare to be
                          // performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-  } else {
-    LoadCell.setCalFactor(calibrationValue);  // set calibration factor (float)
-    Serial.println("Startup is complete");
+  byte loadcell_1_rdy = 0;
+  byte loadcell_2_rdy = 0;
+  while (
+      (loadcell_1_rdy + loadcell_2_rdy) <
+      2) {  // run startup, stabilization and tare, both modules simultaniously
+    if (!loadcell_1_rdy)
+      loadcell_1_rdy = LoadCell_1.startMultiple(stabilizingtime, _tare);
+    if (!loadcell_2_rdy)
+      loadcell_2_rdy = LoadCell_2.startMultiple(stabilizingtime, _tare);
   }
-  while (!LoadCell.update());
-  Serial.print("Calibration value: ");
-  Serial.println(LoadCell.getCalFactor());
-  Serial.print("HX711 measured conversion time ms: ");
-  Serial.println(LoadCell.getConversionTime());
-  Serial.print("HX711 measured sampling rate HZ: ");
-  Serial.println(LoadCell.getSPS());
-  Serial.print("HX711 measured settlingtime ms: ");
-  Serial.println(LoadCell.getSettlingTime());
-  Serial.println(
-      "Note that the settling time may increase significantly if you use "
-      "delay() in your sketch!");
-  if (LoadCell.getSPS() < 7) {
-    Serial.println(
-        "!!Sampling rate is lower than specification, check MCU>HX711 wiring "
-        "and pin designations");
-  } else if (LoadCell.getSPS() > 100) {
-    Serial.println(
-        "!!Sampling rate is higher than specification, check MCU>HX711 wiring "
-        "and pin designations");
+  if (LoadCell_1.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 no.1 wiring and pin designations");
   }
+  if (LoadCell_2.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 no.2 wiring and pin designations");
+  }
+  LoadCell_1.setCalFactor(
+      calibrationValue_1);  // user set calibration value (float)
+  LoadCell_2.setCalFactor(
+      calibrationValue_2);  // user set calibration value (float)
+  Serial.println("Startup is complete");
 }
 
 void loop() {
   static boolean newDataReady = 0;
   const int serialPrintInterval =
-      500;  // increase value to slow down serial print activity
+      0;  // increase value to slow down serial print activity
 
   // check for new data/start next conversion:
-  if (LoadCell.update()) newDataReady = true;
+  if (LoadCell_1.update()) newDataReady = true;
+  LoadCell_2.update();
 
-  // get smoothed value from the dataset:
-  if (newDataReady) {
+  // get smoothed value from data set
+  if ((newDataReady)) {
     if (millis() > t + serialPrintInterval) {
-      float i = LoadCell.getData();
-      Serial.print("Load_cell output val: ");
-      Serial.println(i);
+      float a = LoadCell_1.getData();
+      float b = LoadCell_2.getData();
+      Serial.print("Load_cell 1 output val: ");
+      Serial.print(a);
+      Serial.print("    Load_cell 2 output val: ");
+      Serial.println(b);
       newDataReady = 0;
       t = millis();
     }
@@ -106,11 +101,17 @@ void loop() {
   // receive command from serial terminal, send 't' to initiate tare operation:
   if (Serial.available() > 0) {
     char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay();
+    if (inByte == 't') {
+      LoadCell_1.tareNoDelay();
+      LoadCell_2.tareNoDelay();
+    }
   }
 
-  // check if last tare operation is complete:
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
+  // check if last tare operation is complete
+  if (LoadCell_1.getTareStatus() == true) {
+    Serial.println("Tare load cell 1 complete");
+  }
+  if (LoadCell_2.getTareStatus() == true) {
+    Serial.println("Tare load cell 2 complete");
   }
 }
